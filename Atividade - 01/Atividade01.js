1a8e9a090c48c5593d74836cde044d215c5290f2
SamiraHolanda <137845751+SamiraHolanda@users.noreply.github.com>
2023-06-26 22:22:50 -0300

    Add files via upload
    Atividade 01 - 26/06/2023

---
+++ Atividade - 01/Atividade01.js
@@ -0,0 +1,54 @@
+//Number
+var n1 = 5;
+console.log(n1);
+
+var n2 = 8;
+console.log(n2);
+
+//String
+var filmes = ('Meu malvado favorito', 'A espera de um milagre');
+console.log(filmes);
+
+var nome = ("Ana", "Diego");
+console.log(nome);
+
+//Boolean
+var verdadeiro = true; 
+var falso = false;
+
+console.log("Gosta de goiaba? ");
+console.log(falso);
+
+console.log("Gosta de dormir? ");
+console.log(verdadeiro);
+
+//Array
+var compras = ['arroz', 'macarrão', 'feijão'];
+console.log(compras);
+
+var lista_nomes = ['Helena', 'Luisa', 'Luiz'];
+console.log(lista_nomes);
+
+//Object
+var pessoas = {nome : 'Bia', sobrenome : 'Lima', idade : 28};
+console.log(pessoas.idade);
+console.log(pessoas.nome);
+console.log(pessoas.sobrenome);
+
+var camisa = {tecido : 'algodão', cor : 'laranja'};
+console.log(camisa);
+console.log(camisa.tecido);
+
+//Undefined
+var valor1;
+console.log("nenhum_valorAtribuido" + valor1);
+
+var sem_valor;
+console.log("Nenhum valor atribuído" + sem_valor);
+
+//Null
+var valor2 = null;
+console.log("valor_nulo" + valor2);
+
+var num_nulo = null;
+console.log("numero_nulo" + num_nulo);
---
+++ Atividade - 01/Atividade02.js
@@ -0,0 +1,11 @@
+//Usando var
+var valores = 8;
+var nome = Erica;
+
+//Usando let
+let fruta = maça;
+let flores = rosa;
+
+//Usando const
+const cor = amarelo;
+const roupa = vestido;
